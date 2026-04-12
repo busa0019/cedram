f849d4240f04e00c3ceb4fcd22333c6da493aaea
busa0019 <busa0019@algonquinlive.com>
2026-04-12 11:52:10 -0400

    testing

--- backend/server.js
+++ backend/server.js
@@ -63,8 +63,9 @@ app.get("/", (req, res) => {
   res.send("Research Platform API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on port ${PORT}`);
+// });
+module.exports = app;
